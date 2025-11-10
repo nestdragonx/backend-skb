9cbfd499bbdb246ac8ecf2372617365f33611826
nestdragonx <nestdragonx@gmail.com>
2025-11-09 19:30:35 -0800

    second commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5500', // Frontend URL
+  origin: 'frontend-skb.vercel.app', // Frontend URL
   credentials: true // Allow cookies
 }));
 
